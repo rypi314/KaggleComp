c6441a064a57bf164cb19af9d01a37e9318c8301
Doughty, Ryan <rypi314765@gmail.com>
2022-05-05 19:31:59 -0400

    Created male child feature

--- Titanic/loadFeatures.py
+++ Titanic/loadFeatures.py
@@ -1,5 +1,10 @@
 # process.py
 
+def maleChild(row):
+    if row['Age'] < 10 and row['Sex'] == 'male':
+        return 1
+    return 0
+
 def loadTitanic(fileString):
     import pandas as pd
     dfSource = pd.read_csv(fileString)
@@ -10,6 +15,8 @@ def loadTitanic(fileString):
 
     dfSource['Age'].fillna(dfSource['Age'].mode()[0], inplace=True)
 
+    dfSource['MaleChild'] = dfSource.apply(lambda row: maleChild(row), axis=1)
+
     dfSource['TopDeck'] = dfSource.Cabin.str[0].isin(['A', 'B', 'C', 'D'])
     
     dfFeature = dfSource.join(oneHotSex)
@@ -17,7 +24,7 @@ def loadTitanic(fileString):
     dfFeature = dfFeature.join(oneHotEmbarked)
 
 
-    X = dfFeature[['Pclass','Sex_female','TopDeck', 'SibSp']]
+    X = dfFeature[['Pclass', 'Age', 'Sex_female', 'MaleChild','TopDeck', 'SibSp']]
     X.to_csv('Featured_'+fileString)
     
     try:
